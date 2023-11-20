2f5cd863896c46b5760d5deed9c27b9ccd2d32be
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2023-11-21 03:35:18 +0500

    Create AccountDetail.cs

---
+++ Question2/AccountDetail.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentForm {
+    public class AccountDetail {
+        public string account_num { get; set; }
+        public string password { get; set; }
+        public int amount { get; set; }
+
+    }
+}
